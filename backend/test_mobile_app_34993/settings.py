f8213cf704e1e541ef8e3078ad9b526af464d475
pbcodebot <pooja.bijalwan@crowdbotics.com>
2022-05-09 16:46:08 +0530

    add session middleware

--- backend/test_mobile_app_34993/settings.py
+++ backend/test_mobile_app_34993/settings.py
@@ -264,5 +264,7 @@ REST_FRAMEWORK = {
             'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
             'DEFAULT_AUTHENTICATION_CLASSES': (
                 'rest_framework.authentication.TokenAuthentication',
+                'rest_framework.authentication.SessionAuthentication'
             )
 }
+STRIPE_CURRENCY = "inr"
